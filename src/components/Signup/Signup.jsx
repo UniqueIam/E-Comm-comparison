8011a3124441d2ff8a7eba5b8c25c2e2e4ac6b98
Abhimanyu Kumar <abhimanyukumar83097@gmail.com>
2024-05-22 16:43:34 +0530

    signup page edited

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,16 +1,47 @@
 import React from 'react'
 import './Signup.css'
+import { FaGoogle } from "react-icons/fa";
+import {Link} from 'react-router-dom';
 
 function SignUp() {
+
+
   return (
    <>
     <div className='signup-portion'>
       <img src='./images/image1.svg' alt='image' id='cartImage' />
       <div className='account-section'>
         <h3>Create an <span>Account</span></h3><br/>
-        <p>Enter your detail below</p>
+        <p>Enter your details below</p><br/><br/>
+        <input 
+          type='text'
+          placeholder='Name'
+          required
+          id='name-field'
+          autoComplete='off'
+        /><br/><br/><br/>
+        <input 
+          type='email'
+          placeholder='Email or Phone Number'
+          required
+          id='email-field'
+          autoComplete='off'
+        /><br/><br/><br/>
+          <input 
+          type='password'
+          placeholder='Password'
+          required
+          id='password-field'
+          autoComplete='off'
+        /><br/><br/>
+        <button id='account'>Create Account</button><br/><br/>
+        <button id='signup-google'><FaGoogle id='google-icon' />
+          Sign up with Google
+          </button><br/><br/>
+         <p>Already have account? <span><Link to='/login' id='login-link'> Log in</Link></span></p>
       </div>
-    </div>
+      </div>
+    
    </>
   )
 }
