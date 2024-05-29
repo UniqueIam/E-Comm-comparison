11844b6085e2524f300072a35ad2a4d960f5f7c5
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2024-05-29 20:51:46 +0530

    footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Contact.css'
+import { FaPhone } from 'react-icons/fa';
 
 function Contact() {
   return (
     <>
       <div className='contact-portion'>
         <div className='left-portion'>
-           <h2 id='contact-heading'>Connect With Our Team</h2><br/>
-           lorem
+           <h2 id='contact-heading'>Connect With Our Team</h2>
+             <div className='callus'>
+               <FaPhone id='call'/><h3 className=''>Call To Us</h3>
+              </div>
         </div>
         
       </div>
