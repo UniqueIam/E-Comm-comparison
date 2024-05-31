e995fc0ae775149e745a09dfb73431e389497766
Abhimanyu Kumar <abhimanyukumar83097@gmail.com>
2024-05-31 09:12:09 +0530

    remove conflicts from Login Page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
        <img src='./images/image1.svg' alt='image' id='cartImage' />
         <div className='account-section-login'>
         <h2>Log in to <span> PricePeek</span></h2>
-        <p>Enter your details below</p><br/><br/>
+        <p id='details'>Enter your details below</p><br/><br/>
         <input 
           onChange={e=>(setEmail(e.target.value))}
           value={email}
